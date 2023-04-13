7d3dc7fafad06cb6deb00fd4c6bf688535b3b4b1
Laurie2023 <129828541+Laurie2023@users.noreply.github.com>
2023-04-13 14:23:16 +0200

    commit 1

---
+++ 00_journalists.rb
@@ -0,0 +1,269 @@
+# DATABASE
+
+def list #liste
+  journalists_list = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+  return journalists_list
+end
+
+def better_list(journalists, number_journalists) #création d'une array sans les "@" des handle
+  better_journalists_list = []
+  number_journalists.times do |i|
+    better_journalists_list[i] = ((journalists[i])[1..-1])
+  end 
+  return better_journalists_list
+end 
+
+# FONCTIONS CORRESPONDANT AUX QUESTIONS
+
+def number(journalists) #1.compte le nombre d'élements d'une liste
+  number_journalists = journalists.length()
+  return number_journalists
+end
+
+def shortest(journalists,number_journalists) #2. détermine le plus court handle de la liste
+  length_handle = 0
+  shortest_handle_number = 100
+  shortest_handle_name = ""
+  number_journalists.times do |i|
+    length_handle = journalists[i].length()
+    if length_handle < shortest_handle_number
+      shortest_handle_number = length_handle
+      shortest_handle_name = journalists[i]
+    end
+  end
+  results = [shortest_handle_name,shortest_handle_number]
+  return results
+end
+
+def longuest(journalists,number_journalists)  #détermine le plus long handle de la liste
+  length_handle = 0
+  longuest_handle = 0
+  number_journalists.times do |i|
+    length_handle = journalists[i].length()
+    if length_handle > longuest_handle
+      longuest_handle = length_handle
+    end
+  end
+  return longuest_handle
+end
+
+def five_carac(journalists,number_journalists) #3. détermine le nb de handle avec 5 caractères
+  length_handle = 0
+  count = 0
+  number_journalists.times do |i|
+    length_handle = journalists[i].length()
+    if length_handle == 5
+      count = count +1
+    end
+  end
+  return count
+end
+
+def capital_letter(journalists,number_journalists) #4. détermine le nb de handle commençant par une majuscule
+  count = 0
+  number_journalists.times do |a|
+    if (journalists[a].start_with?(/[A-Z]/)) == true 
+      count = count +1
+    end
+  end
+  return count
+end 
+
+def ordonned_list_name(journalists_list) #5. trie par ordre alphabétique des handles
+  journalists_list_sort = journalists_list.sort_by{|word|word.downcase}
+  return journalists_list_sort
+end 
+
+def ordonned_list_length(journalists_list) #6. trie par ordre longueur des handles
+  journalists_list_sort = journalists_list.sort_by(&:length)
+  return journalists_list_sort
+end 
+
+def position(journalists_list) #7. renvoi la position d'un handle donné
+  puts "De quel handle souhaites-tu savoir la position ? (exemple : @epenser)"
+  print "> "
+  user_handle = gets.chomp
+  position = journalists_list.index(user_handle)
+  return position
+end
+
+def length_cat(journalists, number_journalists,longuest) #r8. envoi le nombre de handles par taille
+  array_length = []#array avec les longueurs
+  longuest.times do |x|
+    array_length[x] = x+1 
+  end
+  array_count = [] #array qui aura les compteurs
+  longuest.times do |x|
+    array_count[x] = 0
+  end
+
+  number_journalists.times do |i|
+    longuest.times do |a|
+      if journalists[i].length() == (a+1)
+        array_count[a] = (array_count[a].to_i)+1
+      end
+    end
+  end
+
+  array_global = [array_length,array_count]
+  return array_global
+end
+
+# MISE EN FORME DU MENU
+
+def question_1
+  puts "Il y a #{number(list)} journalistes dans la liste."
+end
+
+def question_2
+  puts "Le plus petit handle de la liste comporte #{shortest(better_list(list,number(list)),number(list))[1]} caractères (sans compter le @). Il s'agit de @#{shortest(better_list(list,number(list)),number(list))[0]}"
+end
+
+def question_3
+  puts "Il y a #{five_carac(better_list(list,number(list)),number(list))} handle comportant 5 caractères (sans compter le @)."
+end
+
+def question_4
+  puts "Il y a #{capital_letter(better_list(list,number(list)),number(list))} handles qui commence par une majuscule."
+end 
+
+def question_5
+  puts "Voici la liste des handles triés par ordre alphabétique : #{ordonned_list_name(list)}"
+end 
+
+def question_6
+  puts "Voici la liste des handles triés par ordre longueur de handle : #{ordonned_list_length(list)}"
+end 
+
+def question_7
+  puts "Le handle se trouver en position n°#{position(list)} - attention, notre liste commence à la position n°0."
+end 
+
+def question_8 
+  longuest(better_list(list,number(list)),number(list)).times do |i|
+    puts "Il y a  #{length_cat(better_list(list,number(list)), number(list),longuest(better_list(list,number(list)),number(list)))[1][i]} handle(s) comportant #{length_cat(better_list(list,number(list)), number(list),longuest(better_list(list,number(list)),number(list)))[0][i]} caractère(s) (sans prendre en compte le @)."
+  end 
+end 
+
+def intro 
+  puts "Tapes le nombre correspondant à l'information qui t'intéresse :"
+  puts "1/ Connaitre le nombre de handle de la liste."
+  puts "2/ Connaitre le nom du handle le plus court et sa taille."
+  puts "3/ Connaitre le nombre de handle contenant 5 caractères."
+  puts "4/ Connaitre le nombre de handle commençant par une majuscule."
+  puts "5/ Voir la liste, ordonnée suivant l'ordre alphabétique."
+  puts "6/ Voir la liste, ordonnée suivant la longueur des handle."
+  puts "7/ Connaître la position du handle dans la liste."
+  puts "8/ Voir le nombre de handles par taille."
+  puts "9/ Quitter le programme."
+  puts ""
+  print "> "
+  user_choice = gets.chomp.to_i
+  return user_choice
+end 
+
+# MAIN 
+
+def perform # MAIN -- Affiche les informations demandées
+  puts ""
+  puts "---- BIENVENUE DANS LE PROGRAMMES D'ANALYSES DES HANDLE ----"
+  user_choice = 0
+  check = 0
+
+  while check == 0
+    puts ""
+    user_choice = intro
+    puts ""
+    if user_choice == 1
+      question_1
+      puts "Veux-tu connaître une autre information ? (Oui/Non)"
+      print "> "
+      user_choice_bis = gets.chomp.to_s
+      if user_choice_bis.downcase == "non"
+        check = 1
+      end 
+    else 
+      if user_choice == 2
+        question_2
+        puts "Veux-tu connaître une autre information ? (Oui/Non)"
+        print "> "
+        user_choice_bis = gets.chomp.to_s
+        if user_choice_bis.downcase == "non"
+          check = 1
+        end 
+      else 
+        if user_choice == 3
+          question_3
+          puts "Veux-tu connaître une autre information ? (Oui/Non)"
+          print "> "
+          user_choice_bis = gets.chomp.to_s
+          if user_choice_bis.downcase == "non"
+            check = 1
+          end 
+        else 
+          if user_choice == 4
+            question_4
+            puts "Veux-tu connaître une autre information ? (Oui/Non)"
+            print "> "
+            user_choice_bis = gets.chomp.to_s
+            if user_choice_bis.downcase == "non"
+              check = 1
+            end 
+          else 
+            if user_choice == 5
+              question_5
+              puts "Veux-tu connaître une autre information ? (Oui/Non)"
+              print "> "
+              user_choice_bis = gets.chomp.to_s
+              if user_choice_bis.downcase == "non"
+                check = 1
+              end
+            else 
+              if user_choice == 6
+                question_6
+                puts "Veux-tu connaître une autre information ? (Oui/Non)"
+                print "> "
+                user_choice_bis = gets.chomp.to_s
+                if user_choice_bis.downcase == "non"
+                  check = 1
+                end
+              else 
+                if user_choice == 7
+                  question_7
+                  puts "Veux-tu connaître une autre information ? (Oui/Non)"
+                  print "> "
+                  user_choice_bis = gets.chomp.to_s
+                  if user_choice_bis.downcase == "non"
+                    check = 1
+                  end
+                else 
+                  if user_choice == 8
+                    question_8
+                    puts""
+                    puts "Veux-tu connaître une autre information ? (Oui/Non)"
+                    print "> "
+                    user_choice_bis = gets.chomp.to_s
+                    if user_choice_bis.downcase == "non"
+                     check = 1
+                    end
+                  else 
+                    if user_choice == 9
+                      check = 1
+                    else
+                    puts "Merci d'entrer un nombre entre 1 et 8."
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  puts ""
+  puts "---- A BIENTOT ----"
+  
+end
+
+perform
